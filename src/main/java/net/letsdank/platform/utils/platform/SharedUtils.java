037c5034a464a63d20613a069f93ddcdfd3faddb
LetsDank <ichi2209@icloud.com>
2024-05-25 14:23:00 +0300

    Fixes to run app

--- src/main/java/net/letsdank/platform/utils/platform/SharedUtils.java
+++ src/main/java/net/letsdank/platform/utils/platform/SharedUtils.java
@@ -3,6 +3,6 @@ package net.letsdank.platform.utils.platform;
 // Alias: ОбщегоНазначения
 public class SharedUtils {
     public static String getSafeCommandLine(String command) {
-
+        return ""; // TODO: Implement
     }
 }
